4e014a1ced04d0dac3c44f79bb629ab4c5810f94
Pim <pim_melman@icloud.com>
2021-07-22 12:28:48 +0200

    Basic Arrow Functions

---
+++ basicArrowFunctions/js/script.js
@@ -0,0 +1,19 @@
+// zet de functie om naar een arrow functie
+let ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+
+ikRockArrowFunctions()
+
+
+const fivePlusSeven = () => 5 + 7
+
+fivePlusSeven();
+
+
+let myFunction = (a, b) => a + b
+
+
+let addFive = a => a + 5
+
+
+let createObject = () => ({ greeting: "hoi" })
+
