ef2d1c31e26349cf4c0e00daf6f75eed63cf6486
MelmanPim <59539663+MelmanPim@users.noreply.github.com>
2021-07-29 10:46:48 +0200

    Add files via upload

---
+++ arrayMethods3/js/script.js
@@ -0,0 +1,191 @@
+const superheroes = [
+    {
+        "name": "Batman",
+        "publisher": "DC Comics",
+        "alter_ego": "Bruce Wayne",
+        "first_appearance": "Detective Comics #27",
+        "weight": "210"
+    },
+    {
+        "name": "Superman",
+        "publisher": "DC Comics",
+        "alter_ego": "Kal-El",
+        "first_appearance": "Action Comics #1",
+        "weight": "220"
+    },
+    {
+        "name": "Flash",
+        "publisher": "DC Comics",
+        "alter_ego": "Jay Garrick",
+        "first_appearance": "Flash Comics #1",
+        "weight": "195"
+    },
+    {
+        "name": "Green Lantern",
+        "publisher": "DC Comics",
+        "alter_ego": "Alan Scott",
+        "first_appearance": "All-American Comics #16",
+        "weight": "186"
+    },
+    {
+        "name": "Green Arrow",
+        "publisher": "DC Comics",
+        "alter_ego": "Oliver Queen",
+        "first_appearance": "All-American Comics #16",
+        "weight": "195"
+    },
+    {
+        "name": "Wonder Woman",
+        "publisher": "DC Comics",
+        "alter_ego": "Princess Diana",
+        "first_appearance": "The Incredible Hulk #180",
+        "weight": "165"
+    },
+    {
+        "name": "Blue Beetle",
+        "publisher": "DC Comics",
+        "alter_ego": "Dan Garret",
+        "first_appearance": "Mystery Men Comics #1",
+        "weight": "145"
+    },
+    {
+        "name": "Spider Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Peter Parker",
+        "first_appearance": "Amazing Fantasy #15",
+        "weight": "167"
+    },
+    {
+        "name": "Captain America",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Steve Rogers",
+        "first_appearance": "Captain America Comics #1",
+        "weight": "220"
+    },
+    {
+        "name": "Iron Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Tony Stark",
+        "first_appearance": "Tales of Suspense #39",
+        "weight": "250"
+    },
+    {
+        "name": "Thor",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Thor Odinson",
+        "first_appearance": "Journey into Myster #83",
+        "weight": "200"
+    },
+    {
+        "name": "Hulk",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Bruce Banner",
+        "first_appearance": "The Incredible Hulk #1",
+        "weight": "1400"
+    },
+    {
+        "name": "Wolverine",
+        "publisher": "Marvel Comics",
+        "alter_ego": "James Howlett",
+        "first_appearance": "The Incredible Hulk #180",
+        "weight": "200"
+    },
+    {
+        "name": "Daredevil",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Matthew Michael Murdock",
+        "first_appearance": "Daredevil #1",
+        "weight": "200"
+    },
+    {
+        "name": "Silver Surfer",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Norrin Radd",
+        "first_appearance": "The Fantastic Four #48",
+        "weight": "unknown"
+    }
+]
+
+const heroNames = superheroes.map(person => {
+    return person.name;
+});
+console.log(heroNames);
+
+
+
+const lightSuperheroes = superheroes.filter(person => {
+    return person.weight < 190
+});
+
+console.log(lightSuperheroes);
+
+
+const twoHundredPounds = superheroes.filter(person => {
+    return person.weight == 200;
+});
+
+const twoHundredPoundsNames = twoHundredPounds.map(person => {
+    return person.name;
+});
+
+console.log(twoHundredPounds);
+console.log(twoHundredPoundsNames);
+
+
+const firstAppeared = superheroes.map(person => {
+    return person.first_appearance;
+});
+console.log(firstAppeared);
+
+
+
+const DCComics = superheroes.filter(person => {
+    return person.publisher === "DC Comics";
+});
+
+console.log(DCComics);
+
+const MarvelComics = superheroes.filter(person => {
+    return person.publisher === "Marvel Comics";
+});
+
+console.log(MarvelComics);
+
+
+
+const heroesWeightNumberDC = DCComics.map(person => {
+    return parseInt(person.weight);
+});
+
+const heroesWeightNumberMarvel = MarvelComics.map(person => {
+    if (person.weight !== "unknown") {
+        return parseInt(person.weight, 10);
+    } else {
+        return 0;
+    }
+});
+
+const bigSum = function (array) {
+    return array.reduce((addedNumber, nextNumber) => {
+        return addedNumber + nextNumber;
+    });
+};
+
+console.log(bigSum(heroesWeightNumberDC))
+console.log(bigSum(heroesWeightNumberMarvel))
+
+
+
+const weight = superheroes.map(person => {
+    if (person.weight !== "unknown") {
+        return parseInt(person.weight, 10);
+    } else {
+        return 0;
+    }
+});
+
+console.log(Math.max(heroesWeightNumberDC));
+
+
+
+
